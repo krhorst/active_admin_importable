38bf399de78d86720c82ccad77dbb4faaf721021
Kevin Horst <krhorst@gmail.com>
2013-03-26 10:33:07 -0500

    adding ability to pass in optional block to replace default create! behavior

--- app/models/csv_db.rb
+++ app/models/csv_db.rb
@@ -1,11 +1,15 @@
 require 'csv'
 class CsvDb
   class << self
-    def convert_save(model_name, csv_data)
+    def convert_save(model_name, csv_data, &block)
       csv_file = csv_data.read
       CSV.parse(csv_file, :headers => true, header_converters: :symbol ) do |row|
         target_model = model_name.classify.constantize
-        target_model.create!(row.to_hash)
+        if(block_given?)
+            block.call(target_model, row.to_hash)
+         else
+             target_model.create!(row.to_hash)
+         end
       end
     end
   end
--- lib/active_admin_importable/dsl.rb
+++ lib/active_admin_importable/dsl.rb
@@ -1,6 +1,6 @@
 module ActiveAdminImportable
   module DSL
-    def active_admin_importable
+    def active_admin_importable(&block)
       action_item :only => :index do
         link_to "Import #{active_admin_config.resource_name.to_s.pluralize}", :action => 'upload_csv'
       end
@@ -10,7 +10,7 @@ module ActiveAdminImportable
       end
 
       collection_action :import_csv, :method => :post do
-        CsvDb.convert_save(active_admin_config.resource_name.to_s, params[:dump][:file])
+        CsvDb.convert_save(active_admin_config.resource_name.to_s, params[:dump][:file], &block)
         redirect_to :action => :index, :notice => "#{active_admin_config.resource_name.to_s} imported successfully!"
       end
     end
