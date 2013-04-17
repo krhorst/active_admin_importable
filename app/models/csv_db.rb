5875012cc0751d440371bd99e8bdd51d6971b9bb
Kevin Horst <krhorst@gmail.com>
2013-04-17 11:18:52 -0500

    Adding pluralize before constantize to avoid errors in singularizing singular

--- app/models/csv_db.rb
+++ app/models/csv_db.rb
@@ -4,7 +4,7 @@ class CsvDb
     def convert_save(model_name, csv_data, &block)
       csv_file = csv_data.read
       CSV.parse(csv_file, :headers => true, header_converters: :symbol ) do |row|
-        target_model = model_name.classify.constantize
+        target_model = model_name.pluralize.classify.constantize
         if(block_given?)
             block.call(target_model, row.to_hash)
          else
