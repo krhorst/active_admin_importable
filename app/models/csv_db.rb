1dd1e3e2e0c1a43188029a0c77bd189faa424d72
Kevin Horst <krhorst@gmail.com>
2013-04-17 11:22:36 -0500

    Moving classify / constantize outside of CSV parse loop

--- app/models/csv_db.rb
+++ app/models/csv_db.rb
@@ -3,8 +3,8 @@ class CsvDb
   class << self
     def convert_save(model_name, csv_data, &block)
       csv_file = csv_data.read
+      target_model = model_name.pluralize.classify.constantize
       CSV.parse(csv_file, :headers => true, header_converters: :symbol ) do |row|
-        target_model = model_name.pluralize.classify.constantize
         if(block_given?)
             block.call(target_model, row.to_hash)
          else
