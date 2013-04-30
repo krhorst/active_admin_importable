d1247e015caa627d15a3470f255d09c450833aab
Kevin Horst <krhorst@gmail.com>
2013-04-30 11:31:42 -0500

    replacing resource_name with resource_class

--- app/models/csv_db.rb
+++ app/models/csv_db.rb
@@ -1,14 +1,13 @@
 require 'csv'
 class CsvDb
   class << self
-    def convert_save(model_name, csv_data, &block)
+    def convert_save(target_model_klass, csv_data, &block)
       csv_file = csv_data.read
-      target_model = model_name.pluralize.classify.constantize
       CSV.parse(csv_file, :headers => true, header_converters: :symbol ) do |row|
         if(block_given?)
-            block.call(target_model, row.to_hash)
+            block.call(target_model_klass, row.to_hash)
          else
-             target_model.create!(row.to_hash)
+           target_model_klass.create!(row.to_hash)
          end
       end
     end
--- lib/active_admin_importable/version.rb
+++ lib/active_admin_importable/version.rb
@@ -1,3 +1,3 @@
 module ActiveAdminImportable
-  VERSION = "1.1.1"
+  VERSION = "1.1.2"
 end
