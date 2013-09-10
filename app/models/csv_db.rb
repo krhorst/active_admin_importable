2400b69e8b7b3c1a4489ca3413337187d70c6fa4
ukolovda <udmitry@mail.ru>
2013-09-10 14:30:19 +0400

    Make compatible with Ruby 1.8.7.

--- app/models/csv_db.rb
+++ app/models/csv_db.rb
@@ -3,13 +3,13 @@ class CsvDb
   class << self
     def convert_save(target_model, csv_data, role = :default, &block)
       csv_file = csv_data.read
-      CSV.parse(csv_file, :headers => true, header_converters: :symbol ) do |row|
+      CSV.parse(csv_file, :headers => true, :header_converters => :symbol ) do |row|
         data = row.to_hash
         if data.present?
           if (block_given?)
              block.call(target_model, data)
            else
-             target_model.create!(data, as: role)
+             target_model.create!(data, :as => role)
            end
          end
       end
