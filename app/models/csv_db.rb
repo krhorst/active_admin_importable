9cb12315a2b93d8e43a3cd1840038d2b2c64a363
ukolovda <udmitry@mail.ru>
2013-11-07 16:36:25 +0400

    Make compatible with Ruby 1.8.7.

--- app/models/csv_db.rb
+++ app/models/csv_db.rb
@@ -3,7 +3,8 @@ class CsvDb
   class << self
     def convert_save(target_model, csv_data, role = :default, &block)
       csv_file = csv_data.read
-      CSV.parse(csv_file, :headers => true, :header_converters => :symbol ) do |row|
+      parser_class = (RUBY_VERSION=='1.8.7') ? FasterCSV : CSV
+      parser_class.parse(csv_file, :headers => true, :header_converters => :symbol ) do |row|
         data = row.to_hash
         if data.present?
           if (block_given?)
