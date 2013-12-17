87c6742881374f81d5ecc3a020bd9fecde1f5f28
damarinz <damarinz@gmail.com>
2013-12-17 18:45:39 +0900

    Add kconv filter to pass i18n(CJKV) data.

--- app/models/csv_db.rb
+++ app/models/csv_db.rb
@@ -1,8 +1,10 @@
 require 'csv'
+require 'kconv'
 class CsvDb
   class << self
     def convert_save(target_model, csv_data, &block)
       csv_file = csv_data.read
+      csv_file = Kconv.toutf8(csv_file)
       CSV.parse(csv_file, :headers => true, header_converters: :symbol ) do |row|
         data = row.to_hash
         if data.present?
