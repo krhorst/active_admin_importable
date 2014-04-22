4a124ec31d7befc3828538e793be4fe23bfc12e9
Travis Palmer <travis@cramerdev.com>
2014-04-22 12:07:45 -0400

    pass session into custom block

--- lib/active_admin_importable/dsl.rb
+++ lib/active_admin_importable/dsl.rb
@@ -10,7 +10,7 @@ module ActiveAdminImportable
       end
 
       collection_action :import_csv, :method => :post do
-        CsvDb.convert_save(active_admin_config.resource_class, params[:dump][:file], &block)
+        CsvDb.convert_save(active_admin_config.resource_class, params[:dump][:file], session, &block)
         redirect_to :action => :index, :notice => "#{active_admin_config.resource_name.to_s} imported successfully!"
       end
     end
