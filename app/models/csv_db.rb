2d25b8633cd871560fa586d3ec4d512c033ee90d
Kevin Pratt <kevin@ouelong.com>
2015-05-04 14:33:55 -0600

    Added forcing of UTF-8 encoding

--- app/models/csv_db.rb
+++ app/models/csv_db.rb
@@ -3,6 +3,7 @@ class CsvDb
   class << self
     def convert_save(target_model, csv_data, &block)
       csv_file = csv_data.read
+      csv_file.force_encoding(Encoding::UTF_8)
       CSV.parse(csv_file, :headers => true, header_converters: :symbol ) do |row|
         data = row.to_hash
         if data.present?
