a0ba5f20f90e09ba71d73cf75b0de73ccd4c8222
sschafft <scott@bivee.co>
2015-05-17 17:39:34 -0400

    adjusted to remove xls

--- app/models/csv_db.rb
+++ app/models/csv_db.rb
@@ -21,9 +21,8 @@ class CsvDb
 
     def open_spreadsheet(file)
         case File.extname(file.original_filename)
-          when '.csv' then Roo::CSV.new(file.path, nil, :ignore)
-          when '.xls' then Roo::Excel.new(file.path, nil, :ignore)
-          when '.xlsx' then Roo::Excelx.new(file.path, nil, :ignore)
+          when '.csv' then Roo::CSV.new(file.path)
+          when '.xlsx' then Roo::Excelx.new(file.path)
           else raise "Unknown file type: #{file.original_filename}"
         end
     end
--- lib/active_admin_importable/dsl.rb
+++ lib/active_admin_importable/dsl.rb
@@ -2,14 +2,14 @@ module ActiveAdminImportable
   module DSL
     def active_admin_importable(&block)
       action_item :only => :index do
-        link_to "Import #{active_admin_config.resource_name.to_s.pluralize}", :action => 'upload_csv'
+        link_to "Import #{active_admin_config.resource_name.to_s.pluralize}", :action => 'upload_spreadsheet'
       end
 
-      collection_action :upload_csv do
-        render "admin/csv/upload_csv"
+      collection_action :upload_spreadsheet do
+        render "admin/csv/upload_spreadsheet"
       end
 
-      collection_action :import_csv, :method => :post do
+      collection_action :import_spreadsheet, :method => :post do
         CsvDb.convert_save(active_admin_config.resource_class, params[:dump][:file], &block)
         redirect_to :action => :index, :notice => "#{active_admin_config.resource_name.to_s} imported successfully!"
       end
