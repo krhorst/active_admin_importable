912a3aa3bb4590ea231c495eee70c1047ff7d01c
sschafft <scott@bivee.co>
2015-05-17 17:23:27 -0400

    added xlsx support

--- app/models/csv_db.rb
+++ app/models/csv_db.rb
@@ -1,11 +1,15 @@
 require 'csv'
+require 'roo'
+
 class CsvDb
   class << self
-    def convert_save(target_model, csv_data, &block)
-      csv_file = csv_data.read
-      csv_file.force_encoding(Encoding::UTF_8)
-      CSV.parse(csv_file, :headers => true, header_converters: :symbol ) do |row|
-        data = row.to_hash
+    def convert_save(target_model, file, &block)
+      spreadsheet = open_spreadsheet(file)
+      spreadsheet.force_encoding(Encoding::UTF_8)
+
+      header = spreadsheet.row(1)
+      (2..spreadsheet.last_row).each do |i|
+        data = Hash[[header, spreadsheet.row(i)].transpose]
         if data.present?
           if (block_given?)
              block.call(target_model, data)
@@ -13,7 +17,18 @@ class CsvDb
              target_model.create!(data)
            end
          end
+        
       end
     end
+
+    def open_spreadsheet(file)
+        case File.extname(file.original_filename)
+          when '.csv' then Csv.new(file.path, nil, :ignore)
+          when '.xls' then Excel.new(file.path, nil, :ignore)
+          when '.xlsx' then Excelx.new(file.path, nil, :ignore)
+          else raise "Unknown file type: #{file.original_filename}"
+        end
+    end
+
   end
 end
