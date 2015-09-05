be9dcc923f371331f533ad3e053d07f18f510d36
Karl Wilbur <karl@kandrsoftware.com>
2015-09-04 23:42:31 -0400

    Allow for CSV.parse options to be passed into to active_admin_importable.

--- lib/active_admin_importable/dsl.rb
+++ lib/active_admin_importable/dsl.rb
@@ -1,6 +1,6 @@
 module ActiveAdminImportable
   module DSL
-    def active_admin_importable(&block)
+    def active_admin_importable(opts = {}, &block)
       action_item :only => :index do
         link_to "Import #{active_admin_config.resource_name.to_s.pluralize}", :action => 'upload_csv'
       end
@@ -10,7 +10,7 @@ module ActiveAdminImportable
       end
 
       collection_action :import_csv, :method => :post do
-        CsvDb.convert_save(active_admin_config.resource_class, params[:dump][:file], &block)
+        CsvDb.convert_save(active_admin_config.resource_class, params[:dump][:file], opts, &block)
         redirect_to :action => :index, :notice => "#{active_admin_config.resource_name.to_s} imported successfully!"
       end
     end
