feac3292dfc6e9f72e127eb4e28952d45a62004d
Kilian Cirera Sant <kilian@paradem.co>
2016-01-27 15:11:06 +0100

    Support "if" option

--- lib/active_admin_importable/dsl.rb
+++ lib/active_admin_importable/dsl.rb
@@ -1,15 +1,15 @@
 module ActiveAdminImportable
   module DSL
-    def active_admin_importable(&block)
-      action_item :only => :index do
+    def active_admin_importable(options = {}, &block)
+      action_item :only => :index, :if => options[:if] do
         link_to "Import #{active_admin_config.resource_name.to_s.pluralize}", :action => 'upload_spreadsheet'
       end
 
-      collection_action :upload_spreadsheet do
+      collection_action :upload_spreadsheet, :if => options[:if] do
         render "admin/csv/upload_spreadsheet"
       end
 
-      collection_action :import_spreadsheet, :method => :post do
+      collection_action :import_spreadsheet, :method => :post, :if => options[:if] do
         CsvDb.convert_save(active_admin_config.resource_class, params[:dump][:file], &block)
         redirect_to :action => :index, :notice => "#{active_admin_config.resource_name.to_s} imported successfully!"
       end
