514d7fbfd18931bdc9f31f15de55bccb87c002dc
baptou12 <baptou12@gmail.com>
2016-11-13 21:35:55 +0100

    Put importation into an activerecord transaction

--- lib/active_admin_importable/dsl.rb
+++ lib/active_admin_importable/dsl.rb
@@ -10,7 +10,9 @@ module ActiveAdminImportable
       end
 
       collection_action :import_csv, :method => :post do
-        CsvDb.convert_save(active_admin_config.resource_class, params[:dump][:file], &block)
+        ActiveRecord::Base.transaction do
+          CsvDb.convert_save(active_admin_config.resource_class, params[:dump][:file], options[:transaction], &block)
+        end
         redirect_to :action => :index, :notice => "#{active_admin_config.resource_name.to_s} imported successfully!"
       end
     end
