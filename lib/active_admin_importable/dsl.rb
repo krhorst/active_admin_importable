c01674bb40c0676a84c7aa69c998ff0abacdaa64
baptou12 <baptou12@gmail.com>
2016-11-13 21:36:04 +0100

    add options to change name

--- lib/active_admin_importable/dsl.rb
+++ lib/active_admin_importable/dsl.rb
@@ -1,8 +1,9 @@
 module ActiveAdminImportable
   module DSL
-    def active_admin_importable(&block)
+    def active_admin_importable(options={}, &block)
       action_item :only => :index do
-        link_to "Import #{active_admin_config.resource_name.to_s.pluralize}", :action => 'upload_csv'
+        link_name = options[:name] || "Import #{active_admin_config.resource_name.to_s.pluralize}"
+        link_to link_name, :action => 'upload_csv'
       end
 
       collection_action :upload_csv do
@@ -11,7 +12,7 @@ module ActiveAdminImportable
 
       collection_action :import_csv, :method => :post do
         ActiveRecord::Base.transaction do
-          CsvDb.convert_save(active_admin_config.resource_class, params[:dump][:file], options[:transaction], &block)
+          CsvDb.convert_save(active_admin_config.resource_class, params[:dump][:file], &block)
         end
         redirect_to :action => :index, :notice => "#{active_admin_config.resource_name.to_s} imported successfully!"
       end
