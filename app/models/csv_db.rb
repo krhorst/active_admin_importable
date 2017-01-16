3052c908e174b51c298fd7b5846c410d34b8844b
Jason Ayre <jayre@peer60.com>
2017-01-16 12:52:18 -0700

    use rails logger instead of puts

--- app/models/csv_db.rb
+++ app/models/csv_db.rb
@@ -6,14 +6,16 @@ class CsvDb
       csv_file = csv_data.read
       md5 = ::Digest::MD5.new
       md5 << csv_file
-      puts "STARTING IMPORT #{md5}"
+      ::Rails.logger.info("STARTING IMPORT: #{md5}")
       current_row = 0
       failed_rows = []
 
       ::CSV.parse(csv_file, :headers => true, header_converters: :symbol) do |row|
         begin
           current_row += 1
+          ::Rails.logger.info("IMPORT: #{md5} - IMPORTING ROW: #{current_row}")
           data = row.to_hash
+
           if data.present?
             if (block_given?)
                block.call(target_model, data, controller_instance)
@@ -22,13 +24,17 @@ class CsvDb
              end
            end
          rescue ::ArgumentError => e
-           puts "IMPORT ##{md5} FAILED TO PARSE ROW #{current_row}"
+           ::Rails.logger.error("IMPORT: ##{md5} FAILED TO PARSE ROW #{current_row}")
            failed_rows << current_row
-           puts e.inspect
+           ::Rails.logger.error(e.message)
          end
       end
 
-      puts "IMPORT ##{md5} FAILED TO PARSE ROWS #{failed_rows}" if failed_rows.any?
+      if failed_rows.any?
+        ::Rails.logger.error("IMPORT: #{md5} FAILED TO PARSE ROWS #{failed_rows}")
+      else
+        ::Rails.logger.info("IMPORT: #{md5} FINISHED")
+      end
     end
   end
 end
